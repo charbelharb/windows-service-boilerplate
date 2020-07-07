31ed716d8728ff131ac0e02c1ae2b5a9348c5e14
Charbel Harb <s@scorz.org>
2020-07-08 00:16:32 +0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Windows Service Boilerplate with Installer
+
+For Visual Studio 2017+, you will need [Microsoft Visual Studio Installer Projects extension](https://marketplace.visualstudio.com/items?itemName=VisualStudioClient.MicrosoftVisualStudio2017InstallerProjects), and [Microsoft Visual Studio 2015 Installer Projects](https://marketplace.visualstudio.com/items?itemName=visualstudioclient.MicrosoftVisualStudio2015InstallerProjects) for Visual Studio 2015.
---
+++ Service/Service.cs
@@ -0,0 +1,32 @@
+﻿using System.ServiceProcess;
+using System.Timers;
+
+namespace Service
+{
+    public partial class Service : ServiceBase
+    {
+        private Timer timer;
+        private readonly double interval = 1000;
+
+        public Service()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer = new Timer(interval);
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            //Do Work
+        }
+    }
+}
